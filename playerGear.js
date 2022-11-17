9b699fee94fca09349f9a75f4a929d00b687c346
Dddatt <115298848+Dddatt@users.noreply.github.com>
2022-11-17 17:01:06 -0500

    Create playerGear.js

---
+++ playerGear.js
@@ -0,0 +1,374 @@
+window.playerGear={
+    
+    supremeStarAmulet:{
+        
+        mesh:function(box,cylinder,sphere,star){
+            
+            star(0,1.5,0,0.075,0.15,0.025,0.05,0,10,0)
+        }
+    },
+    
+    glider:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        glider:{
+            
+            mesh:function(box){
+                
+                if(player.isGliding){
+                    
+                    box(0,2,0,1,0.2,1.5,[-40,0,0],[1.4,1.4,0])
+                }
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.gliderSpeed=18
+                stats.gliderFall=-5
+            }
+        }
+        
+    },
+    
+    mask:{
+        
+        none:{
+            
+            mesh:function(box){
+                
+                box(-0.1,0.3,0.225,0.05,0.1,0.1,false,[0,0,0])
+                box(0.1,0.3,0.225,0.05,0.1,0.1,false,[0,0,0])
+                box(0,0.15,0.225,0.2,0.05,0.1,false,[0,0,0])
+                box(0,0.5,0,0.55,0.1,0.55,false,[0,0,0])
+            },applyStats:function(){}
+        },
+        
+        gummyMask:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                sphere(0,0.5,0,0.5*1.414,2,1.6,0.4,1.6)
+                cylinder(0,0.65,0,0.26*1.414,0.075,10,0.6,20,2,90,0,0)
+                cylinder(0,0.85,0,0.15,0.2,10,0.2,2,1,90,0,0)
+                cylinder(0,0.35,0,0.52*0.5*1.415,0.45,10,1.6,0.4,1.6,90,0,0)
+                box(-0.1,0.4,0.32,0.05,0.1,0.1,false,[0.2,2,1])
+                box(0.1,0.4,0.32,0.05,0.1,0.1,false,[0.2,2,1])
+                box(0,0.25,0.32,0.2,0.05,0.1,false,[0.2,2,1])
+                
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacityMultiplier*=1.75
+                stats.whiteFieldCapacity*=1.75
+                stats.goo*=1.75
+                stats.redBeeAbilityRate*=1.2
+                stats.blueBeeAbilityRate*=1.2
+                stats.whiteBeeAbilityRate*=1.2
+                stats.bluePollen*=1.35
+                stats.whitePollen*=1.35
+                stats.redPollen*=1.35
+                stats.whitePollen*=1.5
+                stats.instantGooConversion+=0.25
+                stats.pollenFromBees*=1.5
+                stats.honeyFromTokens*=1.5
+                stats.convertRate*=1.75
+                stats.defense+=0.3
+                player.addEffect('gummyMorphPassive')
+                player.addEffect('coinScatterPassive')
+            }
+        },
+        
+        diamondMask:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                sphere(0,0.6,0,0.5*1.414,2,0.3*1.1,1*1.1,2*1.1)
+                cylinder(0,0.65,0,0.28*1.414,0.095,10,100,100,100,90,0,0)
+                cylinder(0,0.35,0,0.52*0.5*1.415,0.45,10,0.3*1.1,1*1.1,2*1.1,90,0,0)
+                box(-0.1,0.4,0.32,0.05,0.1,0.1,false,[100,100,100])
+                box(0.1,0.4,0.32,0.05,0.1,0.1,false,[100,100,100])
+                box(0,0.25,0.32,0.2,0.05,0.1,false,[100,100,100])
+                box(0.2,0.8,0.35,0.05,0.35,0.05,[0,0,15],[100,100,100])
+                box(-0.2,0.8,0.35,0.05,0.35,0.05,[0,0,-15],[100,100,100])
+                box(-0.1,0.8,0.35,0.05,0.35,0.05,[0,0,15],[100,100,100])
+                box(0.1,0.8,0.35,0.05,0.35,0.05,[0,0,-15],[100,100,100])
+                box(0,0.85,0.35,0.3,0.05,0.05,false,[100,100,100])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacityMultiplier*=3
+                stats.blueFieldCapacity*=1.5
+                stats.bluePollen*=1.75
+                stats.convertRate*=50
+                stats.honeyAtHive*=1.1
+                stats.bubblePollen*=1.25
+                stats.blueBombPollen*=1.25
+                stats.redBeeAbilityRate*=1.2
+                stats.blueBeeAbilityRate*=1.2
+                stats.whiteBeeAbilityRate*=1.2
+                stats.defense+=0.3
+                player.addEffect('diamondDrainPassive')
+                player.addEffect('bubbleBombsPassive')
+            }
+        },
+        
+        demonMask:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                sphere(0,0.6,0,0.5*1.414,2,0.25,0,0)
+                cylinder(0,0.65,0,0.28*1.414,0.095,10,1.1,0,0,90,0,0)
+                cylinder(0,0.35,0,0.525*0.5*1.415,0.45,10,0.25,0,0,90,0,0)
+                box(-0.1,0.4,0.32,0.05,0.1,0.1,false,[1.1,0,0])
+                box(0.1,0.4,0.32,0.05,0.1,0.1,false,[1.1,0,0])
+                box(0,0.25,0.32,0.2,0.05,0.1,false,[1.1,0,0])
+                
+                box(-0.3,0.9,0.1,0.13,0.4,0.13,[0,0,20],[1.1,0,0])
+                box(0.3,0.9,0.1,0.13,0.4,0.13,[0,0,-20],[1.1,0,0])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacityMultiplier*=1.75
+                stats.redFieldCapacity*=1.75
+                stats.redPollen*=1.75
+                stats.beeAttack*=1.25
+                stats.redBeeAttack+=2
+                stats.whiteBeeAttack+=1
+                stats.blueBeeAttack+=1
+                stats.instantFlameConversion+=0.5
+                stats.flamePollen*=1.75
+                stats.redBeeAbilityRate*=1.2
+                stats.blueBeeAbilityRate*=1.2
+                stats.whiteBeeAbilityRate*=1.2
+                stats.beeEnergy*=1.25
+                stats.defense+=0.35
+                player.addEffect('xFlamePassive')
+                player.addEffect('ignitePassive')
+            }
+        },
+    },
+    
+    belt:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        honeycombBelt:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                box(0,-0.1,0,0.55,0.085,0.55,false,[1.5,1.4,0])
+                box(0-0.075,-0.1+0.065,0.285,0.1,0.1,0.05,[0,0,20],[5,5,5])
+                box(0+0.075,-0.1+0.065,0.285,0.1,0.1,0.05,[0,0,-20],[5,5,5])
+                box(0,-0.1-0.065,0.285,0.1,0.1,0.05,false,[5,5,5])
+                box(0-0.075,-0.1+0.065,0.265,0.15,0.15,0.05,[0,0,20],[5,5,0])
+                box(0+0.075,-0.1+0.065,0.265,0.15,0.15,0.05,[0,0,-20],[5,5,0])
+                box(0,-0.1-0.065,0.265,0.15,0.15,0.05,false,[5,5,0])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacity+=200000
+                stats.capacityMultiplier*=1.25
+                stats.lootLuck+=1
+                stats.convertRate*=1.75
+                stats.honeyFromTokens*=1.5
+                stats.whiteBeeAttack++
+                stats.whiteBombPollen*=1.3
+                stats.beeEnergy*=1.15
+            }
+        },
+        
+        petalBelt:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                // box(0,-0.1,0,0.55,0.05,0.55,false,[0,1.25,0])
+                box(-0.075*1.5,-0.1+0.05*1.5,0.3,0.2,0.2,0.05,[0,0,30],[1.25,1.25,1.25])
+                box(0.075*1.5,-0.1+0.025*1.5,0.3,0.2,0.2,0.05,[0,0,60],[1.25,1.25,1.25])
+                box(0,-0.1-0.05*1.5,0.3,0.2,0.2,0.05,false,[1.25,1.25,1.25])
+                box(0,-0.1,0.35,0.1,0.1,0.1,[45,45,0],[1.5,1.5,0.2])
+                box(-0.075*1.5,-0.1+0.04*1.5,0.25,0.25,0.25,0.05,[0,0,80],[0,1.25,0])
+                box(0.075*1.5,-0.1+0.025*1.5,0.25,0.25,0.25,0.05,[0,0,20],[0,1.25,0])
+                box(0,-0.1-0.05*1.5,0.25,0.25,0.25,0.05,[0,0,30],[0,1.25,0])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacity+=300000
+                stats.capacityMultiplier*=1.5
+                stats.lootLuck+=1
+                stats.convertRate*=1.8
+                stats.honeyFromTokens*=1.5
+                stats.whiteBeeAttack++
+                stats.whiteBombPollen*=1.5
+                stats.beeEnergy*=1.25
+                player.addEffect('petalStormPassive')
+            }
+        }
+    },
+    
+    backpack:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        pouch:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                sphere(0,0,-0.5,0.6,2,0.9,0.7,0.3)
+                sphere(0,0.3,-0.5,0.2,2,0.9*0.7,0.7*0.7,0.3*0.7)
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacity+=200
+            }
+        },
+        
+        coconutCanister:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                sphere(0,0,-0.6,1.3,2,0.4,0.2,0)
+                sphere(-0.3,0.45,-0.6,0.4,1,0.1,0.05,0)
+                sphere(-0.15,0.4,-0.9,0.4,1,0.1,0.05,0)
+                sphere(0.05,0.5,-0.6,0.4,1,0.1,0.05,0)
+                cylinder(0,0,-0.6,0.3,1.3,10,1.2,1.2,1.2,90,0,90)
+                cylinder(-0.425,0,-0.6,0.2,0.57,10,100,0,0,90,0,90)
+                cylinder(0.425,0,-0.6,0.2,0.57,10,0,0,100,90,0,90)
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.capacity+=2000000
+                stats.convertRate*=25
+                stats.instantRedConversion+=0.15
+                stats.instantBlueConversion+=0.15
+                stats.instantWhiteConversion+=0.25
+                stats.whitePollen*=1.5
+                stats.redPollen*=1.25
+                stats.bluePollen*=1.25
+                stats.whiteBeeAttack+=1
+                stats.redBeeAttack+=1
+                stats.blueBeeAttack+=1
+                stats.defense+=0.1
+                stats.honeyAtHive*=1.1
+                player.addEffect('inspireCoconutsPassive')
+                player.addEffect('emergencyCoconutShieldPassive')
+            }
+        }
+    },
+    
+    boots:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        gummyBoots:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                box(-0.2,-0.5,0.04,0.36,0.15,0.73,false,[0.1*1.75,1*1.75,0.5*1.75])
+                box(0.2,-0.5,0.04,0.36,0.15,0.73,false,[0.1*1.75,1*1.75,0.5*1.75])
+                box(-0.2,-0.35,0,0.325,0.2,0.6,false,[1*1.75,0.2*1.75,1*1.75])
+                box(0.2,-0.35,0,0.325,0.2,0.6,false,[1*1.75,0.2*1.75,1*1.75])
+                box(-0.2,-0.35,0.2,0.15,0.05,0.25,false,[0.1*1.75,1*1.75,0.5*1.75])
+                box(0.2,-0.35,0.2,0.15,0.05,0.25,false,[0.1*1.75,1*1.75,0.5*1.75])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.movementCollection+=15
+                stats.movespeed*=1.275
+                stats.jumpPower*=1.4
+                stats.pollenFromCoconuts*=2
+                stats.goo*=1.25
+                stats.beeSpeed*=1.2
+                stats.honeyFromTokens*=1.25
+                stats.instantGooConversion+=0.15
+                stats.convertRate*=1.75
+                stats.capacityMultiplier*=1.25
+            }
+        },
+        
+        mondoBoots:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                box(-0.2,-0.5,0.04,0.36,0.15,0.73,false,[0,0,1.5])
+                box(0.2,-0.5,0.04,0.36,0.15,0.73,false,[1.5,0,0])
+                box(-0.2,-0.35,0,0.325,0.2,0.6,false,[1.35,1.35,1.35])
+                box(0.2,-0.35,0,0.325,0.2,0.6,false,[1.35,1.35,1.35])
+                box(-0.2,-0.35,0.2,0.15,0.05,0.25,false,[0,0,1.5])
+                box(0.2,-0.35,0.2,0.15,0.05,0.25,false,[1.5,0,0])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.movementCollection+=8
+                stats.movespeed*=1.225
+                stats.jumpPower*=1.35
+                stats.beeSpeed*=1.2
+            }
+        },
+    },
+    
+    leftGuard:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        crimsonGuard:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                box(0.35,0.05,0.1,0.2,0.2,0.2,[0,25,30],[1.5,0,0])
+                box(0.575,0.2,0,0.7,0.08,0.08,[0,25,36],[1.5,1.5,1.5])
+                box(0.5,0.03,0.07,0.4,0.06,0.06,[0,25,-30],[1.5,1.5,1.5])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.convertRate*=1.4
+                stats.redPollen*=1.35
+                stats.capacity+=300000
+                stats.instantRedConversion+=0.1
+                stats.redBombPollen*=1.5
+                stats.redBeeAttack+=2
+                stats.whiteBeeAttack+=1
+                stats.criticalPower+=0.75
+                player.addEffect('focusPulserPassive')
+            }
+        },
+    },
+    
+    rightGuard:{
+        
+        none:{mesh:function(){},applyStats:function(){}},
+        
+        cobaltGuard:{
+            
+            mesh:function(box,cylinder,sphere){
+                
+                box(-0.35,0.05,0.1,0.2,0.2,0.2,[0,-25,-30],[0,0,1.5])
+                box(-0.575,0.2,0,0.7,0.08,0.08,[0,-25,-36],[1.5,1.5,1.5])
+                box(-0.5,0.03,0.07,0.4,0.06,0.06,[0,-25,30],[1.5,1.5,1.5])
+            },
+            
+            applyStats:function(stats,player){
+                
+                stats.convertRate*=1.4
+                stats.capacity+=300000
+                stats.bluePollen*=1.35
+                stats.instantBlueConversion+=0.1
+                stats.blueBombPollen*=1.5
+                stats.blueBeeAttack+=2
+                stats.whiteBeeAttack+=1
+                stats.criticalChance+=0.15
+                player.addEffect('hastePulserPassive')
+            }
+        },
+    }
+}
