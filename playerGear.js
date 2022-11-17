ad5ec0d60a3651a6866da41edcaf06dd3cfb5b3a
Dddatt <115298848+Dddatt@users.noreply.github.com>
2022-11-17 17:11:45 -0500

    Update playerGear.js

--- playerGear.js
+++ playerGear.js
@@ -16,10 +16,7 @@ window.playerGear={
             
             mesh:function(box){
                 
-                if(player.isGliding){
-                    
-                    box(0,2,0,1,0.2,1.5,[-40,0,0],[1.4,1.4,0])
-                }
+                 box(0,2,0,1,0.2,1.5,[-40,0,0],[1.4,1.4,0])
             },
             
             applyStats:function(stats,player){
